3742498661157d2485328548efec262440f298f6
Ashish Gangal <asgangal@microsoft.com>
2017-04-10 21:08:24 -0700

    seperating into data models

---
+++ SharedLibrary/DataModel/Club.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary
+{
+    public class Club
+    {
+        public int id { get; set; }
+        public int resource_state { get; set; }
+        public string name { get; set; }
+        public string profile_medium { get; set; }
+        public string profile { get; set; }
+        public string cover_photo { get; set; }
+        public string cover_photo_small { get; set; }
+        public string sport_type { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public bool @private { get; set; }
+        public int member_count { get; set; }
+        public bool featured { get; set; }
+        public bool verified { get; set; }
+        public string url { get; set; }
+    }
+}
---
+++ SharedLibrary/DataModel/Shoe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary
+{
+    public class Shoe
+    {
+    }
+
+    public class Sho
+    {
+        public string id { get; set; }
+        public bool primary { get; set; }
+        public string name { get; set; }
+        public int resource_state { get; set; }
+        public double distance { get; set; }
+    }
+}
