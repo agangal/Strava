ccf8ab67aca64e0de93786595a7d141e722b0a34
Ashish Gangal <asgangal@microsoft.com>
2017-04-10 21:16:22 -0700

    Moved data to right area path and created a new profile data model;

---
+++ SharedLibrary/DataModel/ProfileDataModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.DataModel
+{
+    public class ProfileDataModel
+    {
+        public Athlete UserAthlete { get; set; }
+    }
+}
---
+++ SharedLibrary/ViewModel/ProfileViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.ViewModel
+{
+    using SharedLibrary.DataModel;
+    public class ProfileViewModel : NotificationBase<ProfileDataModel>
+    {
+
+    }
+}
