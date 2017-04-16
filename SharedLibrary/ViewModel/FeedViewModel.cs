935064332e8971f4d717f83f566fd68966b0060d
Ashish Gangal <asgangal@microsoft.com>
2017-04-16 08:45:14 -0700

    Added view model to display feed

---
+++ SharedLibrary/ViewModel/FeedViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.ViewModel
+{
+    using StravaDotNet;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+
+    public class FeedViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public FeedViewModel()
+        {
+            FollowerActivityCollection = new ObservableCollection<ActivitySummary>();
+        }
+        private ObservableCollection<ActivitySummary> _FollowerActivityCollection;
+        public ObservableCollection<ActivitySummary> FollowerActivityCollection
+        {
+            get { return _FollowerActivityCollection; }
+            set
+            {
+                _FollowerActivityCollection = value;
+            }
+        }
+
+        public async void RefreshFeed()
+        {
+            ActivityClient Activity = new ActivityClient();
+            List<ActivitySummary> Summary = await Activity.GetFollowersActivitiesAsync(1, 100);
+            System.Diagnostics.Debug.WriteLine("Summary received");
+        }
+    }
+}
