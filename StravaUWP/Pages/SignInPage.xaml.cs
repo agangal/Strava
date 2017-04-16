cfd67016e7cc7a235835da0b152a55b596ba8b3a
Ashish Gangal <asgangal@microsoft.com>
2017-04-16 12:15:05 -0700

    Added a signin page

---
+++ StravaUWP/Pages/SignInPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using StravaUWP.Helper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace StravaUWP.Pages
+{
+    using SharedLibrary;
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SignInPage : Page
+    {
+        public SignInPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            Results res = await AuthHelper.StravaSigninFlow();
+            if (res.result == ResultStatus.Successful)
+            {
+                Frame RootFrame = Window.Current.Content as Frame;
+                RootFrame.Navigate(typeof(MainPage));
+            }
+        }
+    }
+}
